d8ce1f0a7325f4468b85d2fab348a583fbe95131
Josa Pimenta <josapimenta.design@gmail.com>
2024-04-21 21:48:08 +0100

    Setup project with frontend and backend #1

---
+++ apps/health-project-frontend/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="health-project-frontend" />
+    </div>
+  );
+}
+
+export default App;
